7a15170ce42f116f49d736fc88dbbe7f046fc402
MehmetAlp05 <mehmetalpd7@gmail.com>
2022-08-01 22:05:39 +0300

    context update

--- script.js
+++ script.js
@@ -3,15 +3,11 @@ function myFunction() {
     var y =document.getElementById("menu");
     if (x.style.display === "block") {
       x.style.display = "none";
-      x.style.maxHeight="0";
       y.style.transform="rotate(0deg)";
       y.style.transition = "1s";
-      x.style.transition = "display 0.25s ease-in";
     } else {
       x.style.display = "block";
-      y.style.transform="rotate(180deg)"
+      y.style.transform="rotate(90deg)"
       y.style.transition = "1s";
-      x.style.maxHeight="130px"
-      x.style.transition = "display 0.25s ease-in";
     }
   } 
